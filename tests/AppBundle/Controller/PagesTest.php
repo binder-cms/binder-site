796681c6b12cdc0c5cbd7459e19ae9927d6fa5d7
Ian Phillips <ian@gumstix.com>
2016-12-08 12:47:45 -0800

    Add our first page (index), update the base template, and add tests

---
+++ tests/AppBundle/Controller/PagesTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PagesTest extends WebTestCase
+{
+    public function testIndex_getRootPath_hasExpectedSection()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertNotEmpty($crawler->filter('section#what h2')->text());
+    }
+
+    public function testIndex_getIndexHtml_returnsOkStatus()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/index.html');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndex_getIndexHtml_hasExpectedSection()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/index.html');
+
+        $this->assertNotEmpty($crawler->filter('section#what h2')->text());
+    }
+}
